0426cfc8ac44c410ae1f1f76fbf90a7a126f66e4
Anna Jonsson <aj@cs.umu.se>
2015-04-27 20:43:14 +0200

    Added test for Symbol

---
+++ tests/se/umu/cs/nfl/aj/wta/SymbolTest.java
@@ -0,0 +1,34 @@
+package se.umu.cs.nfl.aj.wta;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SymbolTest {
+
+	Symbol symbol1 = new Symbol("a", 0);
+	Symbol symbol2 = new Symbol("f", 2);
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void shouldBeCreatedTest() {
+		assertNotNull(new Symbol("a", 0));
+	}
+
+	@Test
+	public void shouldBeEqualToASymbolWithTheSameLabelTest() throws Exception {
+		fail();
+	}
+
+
+
+}
