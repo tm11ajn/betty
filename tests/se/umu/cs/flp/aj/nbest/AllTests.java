d7e30f38d083bb0071e4cf6a19259efbaea84085
Anna Jonsson <aj@cs.umu.se>
2017-11-13 11:10:12 +0100

    Removed old Eppstein implementation

--- tests/se/umu/cs/flp/aj/nbest/AllTests.java
+++ tests/se/umu/cs/flp/aj/nbest/AllTests.java
@@ -2,15 +2,14 @@ package se.umu.cs.flp.aj.nbest;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
- 
+
 @RunWith(Suite.class)
-@Suite.SuiteClasses( { 
-	se.umu.cs.flp.aj.eppstein_k_best.graph.AllGraphTests.class,
-	se.umu.cs.flp.aj.nbest.data.AllDataTests.class, 
+@Suite.SuiteClasses( {
+	se.umu.cs.flp.aj.nbest.data.AllDataTests.class,
 	se.umu.cs.flp.aj.wta.AllWTATests.class,
 	se.umu.cs.flp.aj.wta_handlers.AllWTAHandlerTests.class,
 	se.umu.cs.flp.aj.nbest.BestTreesBasicTest.class,
 	se.umu.cs.flp.aj.nbest.BestTreesTest.class})
 public class AllTests {
-	
+
 }
